9d07135ff090780ee9d3518c916b9b2614264027
hellena254 <hellenah243@gmail.com>
2024-07-07 09:19:28 -0700

    add files

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module for canUnlockAll function
+"""
+
+def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be opened.
+    
+    Parameters:
+    boxes (list of list of int): List of lists where each sublist contains keys for other boxes
+    
+    Returns:
+    bool: True if all boxes can be opened, False otherwise
+    """
+    n = len(boxes)
+    unlocked = [False] * n
+    unlocked[0] = True  # The first box is initially unlocked
+    keys = [0]  # Keys we have
+
+    while keys:
+        current_key = keys.pop()
+        for key in boxes[current_key]:
+            if key < n and not unlocked[key]:
+                unlocked[key] = True
+                keys.append(key)
+
+    return all(unlocked)
