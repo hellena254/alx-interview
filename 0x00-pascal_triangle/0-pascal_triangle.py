69c8e605760c1e9ca6311697c047c93d53972fd1
hellena254 <hellenah243@gmail.com>
2024-07-11 03:51:02 -0700

    modify the file

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
+
 """
 Module for pascal_triangle function
 """
 
+
 def pascal_triangle(n):
     """
     Generates Pascal's triangle of size n
-    
+
     Parameters:
     n (int): The size of the Pascal's triangle to generate
-    
+
     Returns:
     list of list of int: Pascal's triangle represented as a list of lists
     """
@@ -24,5 +26,5 @@ def pascal_triangle(n):
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
         row.append(1)
         triangle.append(row)
-    
+
     return triangle
