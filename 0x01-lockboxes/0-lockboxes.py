097f9e97de84e17779d35828ed8d159c23eee58c
hellena254 <hellenah243@gmail.com>
2024-07-11 03:57:55 -0700

    modify the file

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
+
 """
 Module for canUnlockAll function
 """
 
+
 def canUnlockAll(boxes):
     """
     Determines if all boxes can be opened.
-    
+
     Parameters:
-    boxes (list of list of int): List of lists where each sublist contains keys for other boxes
-    
+    boxes (list of list of int): List of lists where each sublist
+    contains keys for other boxes
+
     Returns:
     bool: True if all boxes can be opened, False otherwise
     """
     n = len(boxes)
     unlocked = [False] * n
-    unlocked[0] = True  # The first box is initially unlocked
-    keys = [0]  # Keys we have
+    unlocked[0] = True
+    keys = [0]
 
     while keys:
         current_key = keys.pop()
